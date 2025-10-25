d7dd989db27925b2a84848c109ff31ab3bb3bc46
Rayan Poussuet <rayan.poussuet@epitech.eu>
2025-10-25 16:27:12 +0200

    added base of frontpage

---
+++ portfolio/src/components/Element-Box.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="boxed-text img-wrapper">
+        <a :href="link.length === 0 ? '#' : link" :target="link.length === 0 ? '_self' : '_blank'">
+            <img :src="image" alt="">
+            <h2>{{ title }}</h2><br>
+            <p>{{ text }}</p>
+        </a>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    link: {
+        type: String,
+        default: "#"
+    },
+    image: {
+        type: String,
+        default: "./../images/ditto.jpg"
+    },
+    title: {
+        type: String,
+        default: ""
+    },
+    text: {
+        type: String,
+        default: ""
+    }
+})
+console.log(props.link.length === 0 ? '_blank' : false)
+</script>
---
+++ portfolio/src/vues/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="horizontal-scroll-wrapper">
+        <div v-for="el in frontpage" :key="el['id']" :class="classbox[el['id']]">
+            <ElementBox :link="el['link']" :image="el['image']" :title="el['title']" :text="el['text']" />
+        </div>
+    </div>
+</template>
+
+<script setup>
+import ElementBox from '@/components/Element-Box.vue';
+import { ref } from 'vue';
+import frontpage from "./../../data/frontpage.json"
+
+console.log(frontpage);
+const classbox = ref(["slower", "slower1", "lower2", "slower-down", "slowest", "faster", "faster1", "fastest", "vertical", "last"])
+
+</script>
